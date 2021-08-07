10bd5be2cd25ef5f8f8b96564ad412ab57c8e73c
Bilal Fawaz <bilal.fawaz@student.ehb.be>
2021-08-08 00:28:18 +0200

    API integreren

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 "use strict";
+
+let baseUrl = "https://thecrew.cc/news/read.php";
+
+let articles;
+
+window.onload = onLoad;
+
+
+async function onLoad() {
+
+    let result = await sendRequest();
+    articles = result.news;
+    console.log(articles);
+}
+
+async function sendRequest() {
+    let result = await fetch(baseUrl);
+    return result.json();
+}
